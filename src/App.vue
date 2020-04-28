c1aa680095f05874609eae16ef116affab8ab4f7
sunny <2902252788@qq.com>
2020-04-28 16:50:59 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <index msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import index from './components/index/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    index
+  }
+}
+</script>
+
+<style>
+
+</style>
